afe536312c22bd48f9a7146f52b2292faf43b96c
João Euclides <jesm@ic.ufal.br>
2026-05-04 23:48:32 -0300

    fix: typo no prismaClient.ts

--- src/shared/database/prismaClient.ts
+++ src/shared/database/prismaClient.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from '../../generated/prisma';
 
-// singleton para evitar multiplas conexões em dev 
+// singleton para evitar multiplas conexões em dev
 export const prismaClient = new PrismaClient({
   log: process.env.NODE_ENV === 'development' ? ['query', 'warn', 'error'] : ['error'],
 });
